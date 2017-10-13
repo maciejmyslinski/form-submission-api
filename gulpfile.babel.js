80644fecb04a1a56be17d981d8806b36c9a1987d
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-10-13 18:49:58 +0200

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,14 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
+
+gulp.task('default', () => {
+  gulp
+    .src('index.js')
+    .pipe(eslint())
+    .pipe(eslint.failOnError())
+    .pipe(babel({
+      presets: ['env'],
+    }))
+    .pipe(gulp.dest('build'));
+});
